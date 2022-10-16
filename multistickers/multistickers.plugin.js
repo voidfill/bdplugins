113c344747449b634f1b850a3ed091045eb78fa9
voidfill <71205200+voidfill@users.noreply.github.com>
2022-10-16 12:21:50 +0200

    fix for swc
    now with your favorite, createElement patching c;

--- multistickers/multistickers.plugin.js
+++ multistickers/multistickers.plugin.js
@@ -1,111 +1,127 @@
 /**
  * @name multistickers
  * @description lets you send up to 3 stickers and shift click stickers
- * @version 1.0.0
+ * @version 2.0.0
  * @author void
- * @updateurl https://raw.githubusercontent.com/voidfill/bdplugins/main/multistickers/multistickers.plugin.js
+ * @source https://raw.githubusercontent.com/voidfill/bdplugins/main/multistickers/multistickers.plugin.js
  */
 
-const stickerStore = {};
-const shiftEvent = {
-	shouldAttach: false,
-	shouldNotClose: false,
-};
+const Api = new BdApi("multistickers");
+const { byProps } = Api.Webpack.Filters;
 
-const dispatch = BdApi.findModuleByProps("dirtyDispatch");
-const CloseExpressionPicker = BdApi.findModuleByProps("closeExpressionPicker");
-const ShouldAttachSticker = BdApi.findModuleByProps("shouldAttachSticker");
-const StickerPreview = BdApi.findModuleByProps("getStickerPreview");
-const StickerPreviewContainer = BdApi.findModule((m) => m.type.toString().indexOf("stickerPreviewDivider") !== -1);
-const stickerPreviewContainerCN = BdApi.findModuleByProps("stickerPreviewContainer").stickerPreviewContainer;
-const StickerRow = BdApi.findModule((m) => m.default.type.toString().indexOf("e.isDisplayingIndividualStickers") !== -1).default;
+const [StickerPreviewStore, Dispatch, ShouldAttachSticker, StickerPickerRow, CloseExpressionPicker] = Api.Webpack.getBulk.apply(
+	null,
+	[
+		byProps("getStickerPreview"),
+		byProps("dispatch", "register"),
+		byProps("Hc", "Zt", "Zv", "_V", "Q6"),
+		(m) => m.Z.type.toString().indexOf("e.isDisplayingIndividualStickers") !== -1,
+		byProps("_Q", "PG", "j9", "RO"),
+	].map((f) => (f.filter ? f : { filter: f }))
+);
+
+const previewStore = {
+	lastRequest: null,
+	store: [{}, {}],
+	get(channelId, draftType) {
+		return (draftType === 2 ? this.store[0][channelId] : this.store[1][channelId]) ?? [];
+	},
+	set(channelId, draftType, stickers) {
+		if (draftType === 2) {
+			this.store[0][channelId] = stickers;
+		} else {
+			this.store[1][channelId] = stickers;
+		}
+	},
+};
+const shiftEvent = {
+	shouldAttachSticker: false,
+	shouldNotCloseExpressionPicker: false,
+};
 
 module.exports = class multistickers {
 	start() {
-		BdApi.Patcher.instead("multistickers", ShouldAttachSticker, "shouldAttachSticker", (_, args, originalFunction) => {
-			if (shiftEvent.shouldAttach) {
-				shiftEvent.shouldAttach = false;
+		Api.Patcher.instead(StickerPreviewStore, "getStickerPreview", (_, [channelId, draftType]) => {
+			previewStore.lastRequest = [channelId, draftType];
+			return previewStore.get(channelId, draftType);
+		});
+
+		Api.Patcher.instead(ShouldAttachSticker, "Hc", (_, args, originalFunction) => {
+			if (shiftEvent.shouldAttachSticker) {
+				shiftEvent.shouldAttachSticker = false;
 				return true;
-			} else {
-				return originalFunction.apply(null, args);
 			}
+			return originalFunction.apply(null, args);
 		});
 
-		BdApi.Patcher.instead("multistickers", CloseExpressionPicker, "closeExpressionPicker", (_, __, originalFunction) => {
-			if (shiftEvent.shouldNotClose) {
-				shiftEvent.shouldNotClose = false;
-			} else {
-				originalFunction();
+		Api.Patcher.instead(CloseExpressionPicker, "_Q", (_, args, originalFunction) => {
+			if (shiftEvent.shouldNotCloseExpressionPicker) {
+				shiftEvent.shouldNotCloseExpressionPicker = false;
+				return;
 			}
+			return originalFunction.apply(null, args);
 		});
 
-		BdApi.Patcher.instead("multistickers", StickerPreview, "getStickerPreview", (_, [channelId, draftType]) => {
-			return [...(stickerStore[channelId]?.[draftType] ?? [])];
-		});
+		Api.Patcher.after(Api.React, "createElement", (_, args, ret) => {
+			if (args?.[1]?.className !== "stickerPreviewContainer-U9ZN2r") return ret;
 
-		BdApi.Patcher.after("multistickers", StickerPreviewContainer, "type", (_, [{ channelId, chatInputType }], ret) => {
-			if (!ret) return;
-			const type = chatInputType.drafts.type;
-			const stickers = ret.props.children[0].props.children;
-			for (const sticker of stickers) {
-				const closeButton = sticker?.props?.children?.[0]?.props;
-				if (closeButton) {
-					closeButton.onClick = () => {
-						const index = stickerStore[channelId]?.[type]?.findIndex((s) => s.id === sticker.key);
-						if (index === -1) return;
-						stickerStore[channelId][type].splice(index, 1);
-						StickerPreview.emitChange();
-					};
-				}
-			}
+			const [channelId, draftType] = previewStore.lastRequest;
+			const id = ret.key;
+			ret.props.children[0].props.onClick = () => {
+				previewStore.set(
+					channelId,
+					draftType,
+					previewStore.get(channelId, draftType).filter((s) => s.id !== id)
+				);
+				StickerPreviewStore.emitChange();
+			};
 			return ret;
 		});
 
-		BdApi.Patcher.after("multistickers", StickerRow, "type", (_, __, ret) => {
-			for (const stickerGridcell of ret.props.children) {
-				const cellProps = stickerGridcell.props.children.props;
-				const oldOnClick = cellProps.onClick;
-				cellProps.onClick = (e) => {
-					if (e.shiftKey) {
-						shiftEvent.shouldAttach = true;
-						shiftEvent.shouldNotClose = true;
+		Api.Patcher.after(StickerPickerRow["Z"], "type", (_, __, ret) => {
+			for (const stickerContainer of ret?.props?.children) {
+				const button = stickerContainer.props.children.props;
+				const originalFunction = button.onClick;
+				button.onClick = (event) => {
+					if (event.shiftKey) {
+						shiftEvent.shouldAttachSticker = true;
+						shiftEvent.shouldNotCloseExpressionPicker = true;
 					}
-					oldOnClick(e);
+					originalFunction(event);
 				};
 			}
 		});
 
-		dispatch.subscribe("ADD_STICKER_PREVIEW", this.addStickerPreview);
-		dispatch.subscribe("CLEAR_STICKER_PREVIEW", this.clearAllStickers);
+		Dispatch.subscribe("ADD_STICKER_PREVIEW", this.#addStickerPreview);
+		Dispatch.subscribe("CLEAR_STICKER_PREVIEW", this.#clearStickerPreview);
+		Dispatch.subscribe("LOGOUT", this.#logout);
 
-		BdApi.injectCSS(
-			"multistickers",
-			`.${stickerPreviewContainerCN} {
-			margin-right: 4px;
-		}`
-		);
+		Api.DOM.addStyle(`.closeButton-mGpA26 { margin-right: 2px;}
+.stickerPreview-3hZwLL { margin-right: 8px}`);
 	}
 
-	addStickerPreview({ channelId, draftType, sticker }) {
-		stickerStore[channelId] ??= [];
-		stickerStore[channelId][draftType] ??= [];
-		if (stickerStore[channelId][draftType].some((s) => s === sticker)) return;
-
-		if (stickerStore[channelId][draftType].length == 3) {
-			stickerStore[channelId][draftType].shift();
+	#addStickerPreview({ channelId, draftType, sticker }) {
+		const arr = previewStore.get(channelId, draftType).filter((s) => s.id !== sticker.id);
+		if (arr.length === 3) {
+			arr.shift();
 		}
-		stickerStore[channelId][draftType].push(sticker);
-		StickerPreview.emitChange();
+		previewStore.set(channelId, draftType, [...arr, sticker]);
 	}
 
-	clearAllStickers({ channelId, draftType }) {
-		delete stickerStore[channelId]?.[draftType];
-		StickerPreview.emitChange();
+	#clearStickerPreview({ channelId, draftType }) {
+		previewStore.set(channelId, draftType, []);
+	}
+
+	#logout() {
+		previewStore.store = [{}, {}];
 	}
 
 	stop() {
-		BdApi.Patcher.unpatchAll("multistickers");
-		dispatch.unsubscribe("ADD_STICKER_PREVIEW", this.addStickerPreview);
-		dispatch.unsubscribe("CLEAR_STICKER_PREVIEW", this.clearAllStickers);
+		Dispatch.unsubscribe("ADD_STICKER_PREVIEW", this.#addStickerPreview);
+		Dispatch.unsubscribe("CLEAR_STICKER_PREVIEW", this.#clearStickerPreview);
+		Dispatch.unsubscribe("LOGOUT", this.#logout);
+		Api.Patcher.unpatchAll();
+		Api.DOM.removeStyle();
+		previewStore.store = [{}, {}];
 	}
 };
